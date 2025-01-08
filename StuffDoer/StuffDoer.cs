bead9091d844f55087a4ecc313dccc497186c262
Parker Stovall <parker.stovall@thedataagency.com>
2025-01-08 16:42:19 -0600

    Initial Commit

---
+++ StuffDoer/StuffDoer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StuffDoers
+{
+    public class StuffDoer
+    {
+        public string DoStuff()
+        {
+            return "Stuff Has Been Done!";
+        }
+    }
+}
---
+++ StuffDoerTests/StuffDoerTests.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using StuffDoers;
+
+namespace StuffDoerTests
+{
+    [TestClass]
+    public class StuffDoerTests
+    {
+        [TestMethod]
+        public void StuffDoerDoStuffTest()
+        {
+            var stuffDoer = new StuffDoer();
+            var stuff = stuffDoer.DoStuff();
+            // Assert.AreEqual(stuff, "Stuff Has Been Done!");
+            Assert.IsNotNull(null);
+        }
+    }
+}
